470d6f4d79df3d6c0ff3629d041e59d89274a2e8
PrinceSvelte <prince.rajput@sveltetech.com>
2024-10-29 14:04:41 +0530

    sign in fixes

--- app/components/SignIn.jsx
+++ app/components/SignIn.jsx
@@ -61,24 +61,24 @@ const SignIn = () => {
     };
 
   return (
-    <div class="bg-gray-50 font-[sans-serif]">
-      <div class="min-h-screen flex flex-col items-center justify-center py-6 px-4">
-        <div class="max-w-md w-full">
-          <div class="p-8 rounded-2xl bg-white shadow">
-            <h2 class="text-gray-800 text-center text-2xl font-bold">Sign in</h2>
-            <form class="mt-8 space-y-4 flex flex-col" onSubmit={(e) => handleClick(e)}>
+    <div className="bg-gray-50 font-[sans-serif]">
+      <div className="min-h-screen flex flex-col items-center justify-center py-6 px-4">
+        <div className="max-w-md w-full">
+          <div className="p-8 rounded-2xl bg-white shadow">
+            <h2 className="text-gray-800 text-center text-2xl font-bold">Sign in</h2>
+            <form className="mt-8 space-y-4 flex flex-col" onSubmit={(e) => handleClick(e)}>
               <div>
-                <label class="text-gray-800 text-sm mb-2 block">Email</label>
-                <div class="relative flex items-center">
-                  <input onChange={handleChangeInput} name="email" type="email" required class="w-full text-gray-800 text-sm border border-gray-300 px-4 py-3 rounded-md outline-blue-600" placeholder="Enter user name" />
+                <label className="text-gray-800 text-sm mb-2 block">Email</label>
+                <div className="relative flex items-center">
+                  <input onChange={handleChangeInput} name="email" type="email" required className="w-full text-gray-800 text-sm border border-gray-300 px-4 py-3 rounded-md outline-blue-600" placeholder="Enter user name" />
                
                 </div>
               </div>
 
               <div>
-                <label class="text-gray-800 text-sm mb-2 block">Password</label>
-                <div class="relative flex items-center">
-                  <input onChange={handleChangeInput} name="password" type={passwordType} required class="w-full text-gray-800 text-sm border border-gray-300 px-4 py-3 rounded-md outline-blue-600" placeholder="Enter password" />
+                <label className="text-gray-800 text-sm mb-2 block">Password</label>
+                <div className="relative flex items-center">
+                  <input onChange={handleChangeInput} name="password" type={passwordType} required className="w-full text-gray-800 text-sm border border-gray-300 px-4 py-3 rounded-md outline-blue-600" placeholder="Enter password" />
                   <button
         type="button"
         onClick={togglePassword}
@@ -91,8 +91,8 @@ const SignIn = () => {
                 </div>
               </div>
 
-              <div class="!mt-8">
-                <button type="submit" class="w-full py-3 px-4 text-sm tracking-wide rounded-lg text-white bg-blue-600 hover:bg-blue-700 focus:outline-none">
+              <div className="!mt-8">
+                <button type="submit" className="w-full py-3 px-4 text-sm tracking-wide rounded-lg text-white bg-blue-600 hover:bg-blue-700 focus:outline-none">
                   Sign in
                 </button>
               </div>
